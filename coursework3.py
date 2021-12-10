97f862be7df7ec576c826b8a10a671f4f06b9fce
Tohir Omoloye <omoloye.tohir@yahoo.co.uk>
2021-12-10 18:13:45 +0000

    Updated

--- coursework3.py
+++ coursework3.py
@@ -40,17 +40,62 @@ def wt_zodiac(birthDay, birthMonth):
     return sign;
 
 #A function to calculate the animal associated with a zodiac sign
-def cn_zodiac(year):
-    animal = 'rat';
-    return animal;
+def cn_zodiac(birth_year):
+    if birth_year % 12 == 0:
+        zodiac_animal = 'Monkey'
+    if birth_year % 12 == 1:
+        zodiac_animal = 'Rooster'
+    if birth_year % 12 == 2:
+        zodiac_animal = 'Dog'
+    if birth_year % 12 == 3:
+        zodiac_animal = 'Pig'
+    if birth_year % 12 == 4:
+        zodiac_animal = 'Rat'
+    if birth_year % 12 == 5:
+        zodiac_animal = 'Ox'
+    if birth_year % 12 == 6:
+        zodiac_animal = 'Tiger'
+    if birth_year % 12 == 7:
+        zodiac_animal = 'Rabbit'
+    if birth_year % 12 == 8:
+        zodiac_animal = 'Dragon'
+    if birth_year % 12 == 9:
+        zodiac_animal = 'Snake'
+    if birth_year % 12 == 10:
+        zodiac_animal = 'Horse'
+    if birth_year % 12 == 11:
+        zodiac_animal = 'Sheep'
+
+    return zodiac_animal;
 
 
+def writeToFile(userList):
+    f = open("zodiac.txt", "w")
+
+    #Write all user in table format in a txt file
+    print("Writing user data to zodiac.txt")
+    f.write("UID\t Name\t D.o.b\t Age\t Western Zodiac\t Chinese Zodiac\n")
+    for u in userList:
+        uId     = u[0]
+        uName   = u[1]
+        uDob    = u[2]
+        uAge    = u[3]
+        uSign   = u[4]
+        cnSign   = u[5]
+        f.write(f'{uId:2}  {uName:10} {uDob:12} {uAge:5} {uSign:15} {cnSign:15}')
+        f.write("\n")
+
+    f.close()
+    print("Operation completed successfully")
+
+# Main function. This is the entry point of the program
 def main():
 
     userList = [] # Initialize user list array
     terminate = 0 # Initiase terminate, the progam will keep runing until terminate is set to 1
 
     #Read input from text file
+    print("\nReading data from userData.txt file")
     userData = open("UserData.txt", "r")
     for line in userData:
         #split line by tab space
@@ -60,7 +105,6 @@ def main():
         uId = userArray[0]
         name = userArray[1] + ' ' + userArray[2]
         dob = userArray[3]
-        print(userArray)
             
         #split the date of birth to extract the month, day and year
         splited    = dob.split('-')
@@ -73,22 +117,13 @@ def main():
         #get Zodiac Sign
         sign = wt_zodiac(day, month)
         #get the chinese zodiac sign
-        cnZodiac = cn_zodiac(year)
+        cnZodiac = cn_zodiac(int(year))
 
         # Push user information into user list array
-        userList.append( [uId, name, dob, userAge, sign, cnZodiac]);
+        userList.append( [uId, name, dob, userAge, sign, cnZodiac])
+        print ("processing...")
 
-
-    #display all user in table format
-    print("UID\t Name\t D.o.b\t Age\t Western Zodiac\t Chinese Zodiac\n")
-    for u in userList:
-        uId     = u[0]
-        uName   = u[1]
-        uDob    = u[2]
-        uAge    = u[3]
-        uSign   = u[4]
-        cnSign   = u[5]
-        print(f'{uId:2}  {uName:10} {uDob:12} {uAge:5} {uSign:15} {cnSign:15}')
+    writeToFile(userList) # call the function to write the result to file
 
 if __name__ == "__main__":
     main()
